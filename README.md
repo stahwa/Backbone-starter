11450b4479e4c4cc708ea50205fd7bb4fc0449b0
stahwa <stahwa@gmail.com>
2015-05-01 11:08:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Backbone Starter
+
+
+This is a minimal Backbone starter that includes Browserify, Gulp, Sass, and Handlebars.
+
+
+## Getting started
+
+* Install:
+    * Clone this repo
+    * Run `npm install` in the root directory
+    * Run `bower install` in the root directory
+    
+* Workflow:
+    * Just run `gulp` and you're good to go.
+    * You work in `app/`.
+    * `public/` is the final build.
+
+
+
---
+++ app/main.js
@@ -0,0 +1,24 @@
+
+var Router = require('./router');
+var router = new Router();
+var SiteModel = require('modules/base/site/models/siteModel');
+// var Info = {foo:"bar"}//require('./info')
+
+BB = window.BB || {};
+BB = {
+  models: {},
+  collections: {},
+  views: {},
+  routers: router,
+  init: function() {
+  	this.site = new SiteModel();
+    Backbone.history.start();
+  }
+};
+
+// create pub-sub functionality
+Backbone.pubSub = _.extend({}, Backbone.Events);
+
+$(document).ready(function () {
+	BB.init();
+});
